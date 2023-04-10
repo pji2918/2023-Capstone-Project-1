98e077f1ffa92e7045c4378856ce7799e7d9f716
winterfish_17 <kihara05110605@gmail.com>
2023-04-10 20:28:18 +0900

    몬스터 스크립트 조정

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MonsterController : MonoBehaviour
+public abstract class MonsterController : MonoBehaviour
 {
     [Tooltip("몬스터가 추적할 플레이어의 위치입니다.")] public Vector2 _playerPosition;
     NavMeshAgent _agent;
 
+    SpriteRenderer monsterRenderer;
+
+    public float speed;
+
     // NavMesh 에이전트의 회전과 Z축을 고정시킵니다.
     void Start()
     {
+        monsterRenderer = this.GetComponent<SpriteRenderer>();
         _agent = this.GetComponent<NavMeshAgent>();
         _agent.updateRotation = false;
         _agent.updateUpAxis = false;
@@ -21,5 +26,58 @@ public class MonsterController : MonoBehaviour
     {
         _playerPosition = GameObject.Find("Player").transform.position;
         _agent.SetDestination(_playerPosition);
+
+        if (transform.position.x > _playerPosition.x)
+        {
+            monsterRenderer.flipX = false;
+        }
+        else if (transform.position.x < _playerPosition.x)
+        {
+            monsterRenderer.flipX = true;
+        }
+    }
+
+    public abstract void Attack();
+}
+
+public class SnakeFish : MonsterController
+{
+    //speed = 5f;
+
+    public override void Attack()
+    {
+
+    }
+}
+
+public class Octopus : MonsterController
+{
+    public override void Attack()
+    {
+
+    }
+}
+
+public class MuteBear : MonsterController
+{
+    public override void Attack()
+    {
+
+    }
+}
+
+public class MuteHuman : MonsterController
+{
+    public override void Attack()
+    {
+
+    }
+}
+
+public class LightFish : MonsterController
+{
+    public override void Attack()
+    {
+
     }
 }
