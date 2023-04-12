040c9270301cbd6ac49bd5fd747eb04a22f9ce56
ask217 <qwerask2725@naver.com>
2023-04-12 20:08:08 +0900

    로비씬 UI 창띄우기 다음 씬으로 넘기기

--- Assets/Scripts/Management/UIManager.cs
+++ Assets/Scripts/Management/UIManager.cs
@@ -1,16 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    void Start()
+
+    [SerializeField]
+    private GameObject weaponWindow;
+    [SerializeField]
+    private GameObject upgradeWindow;
+    [SerializeField]
+    private GameObject bookWindow;
+
+    #region 버튼 함수
+    public void OnClickWeaponButton()
     {
-        
+        WindowPopUp(weaponWindow);
     }
 
-    void Update()
+    public void OnClickUpgradeButton()
     {
-        
+        WindowPopUp(upgradeWindow);
+    }
+
+    public void OnClickTableButton()
+    {
+        WindowPopUp(bookWindow);
+    }
+
+    public void OnClickWeaponBackground()
+    {
+        WindowDisappear(weaponWindow);
+    }
+
+    public void OnClickUpgradeBackground()
+    {
+        WindowDisappear(upgradeWindow);
+    }
+
+    public void OnClickTableBackground()
+    {
+        WindowDisappear(bookWindow);
+    }
+    #endregion
+
+    void PopUpWidowChange(GameObject popUp, GameObject window)
+    {
+        popUp.SetActive(!popUp.activeSelf);
+        window.SetActive(!window.activeSelf);
+    }
+
+    void WindowDisappear(GameObject popUp)
+    {
+        popUp.SetActive(false);
+    }
+
+    void WindowPopUp(GameObject popUp)
+    {
+        popUp.SetActive(true);
+    }
+
+    public void HomeToFighting()
+    {
+        SceneManager.LoadScene("Fighting");
     }
 }
