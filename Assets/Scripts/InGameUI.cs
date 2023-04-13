c011fb384feb74592a44da85e039742f9964ab68
pji2918 <son77391@gmail.com>
2023-04-13 19:51:40 +0900

    툴팁 거의 완성

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -8,6 +8,8 @@ public class InGameUI : MonoBehaviour
     public Image _healthBar;
 
     public Image _dashCoolDownBar;
+    private bool[] _isHovering = new bool[4] { false, false, false, false };
+    public GameObject[] _skillTooltip;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +22,24 @@ public class InGameUI : MonoBehaviour
     {
         _healthBar.fillAmount = PlayerController.instance._playerHp / 100f;
         _dashCoolDownBar.fillAmount = PlayerController.instance._dashCoolDown / 10f;
+
+        if (_isHovering[0])
+        {
+            _skillTooltip[0].SetActive(true);
+        }
+        else
+        {
+            _skillTooltip[0].SetActive(false);
+        }
+    }
+
+    public void OnHoverOne()
+    {
+        _isHovering[0] = true;
+    }
+
+    public void OnNotHoverOne()
+    {
+        _isHovering[0] = false;
     }
 }
