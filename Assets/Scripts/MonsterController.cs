a6523574e25892d2a60f341821d738d9b2a77a29
pji2918 <son77391@gmail.com>
2023-04-22 16:03:47 +0900

    스킬 4개 완성

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -7,6 +7,7 @@ public class MonsterController : MonoBehaviour
 {
     [Tooltip("몬스터가 추적할 플레이어의 위치입니다.")] public Vector2 _playerPosition;
     NavMeshAgent _agent;
+    private bool _isKnockback = false;
 
     // NavMesh 에이전트의 회전과 Z축을 고정시킵니다.
     void Start()
@@ -19,7 +20,50 @@ public class MonsterController : MonoBehaviour
     // 플레이어의 위치를 변수에 저장한 다음, 에이전트의 목적지를 플레이어의 위치로 설정합니다.
     void Update()
     {
-        _playerPosition = GameObject.Find("Player").transform.position;
-        _agent.SetDestination(_playerPosition);
+        if (!_isKnockback)
+        {
+            _playerPosition = GameObject.Find("Player").transform.position;
+            _agent.SetDestination(_playerPosition);
+        }
+        if (!PlayerController.instance._jangpungOn)
+        {
+            _isKnockback = false;
+            this.GetComponent<NavMeshAgent>().enabled = true;
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Bubble"))
+        {
+            // 여기에 몬스터가 비눗방울에 닿았을 때 HP가 서서히 감소하는 코드를 작성합니다.
+        }
+    }
+
+    void OnTriggerEnter2D(Collider other)
+    {
+        if (other.CompareTag("Harpoon"))
+        {
+            // 여기에 몬스터가 작살에 닿았을 때 HP가 감소하는 코드를 작성합니다.
+        }
+        if (other.name == "Attack_Effect")
+        {
+            // 여기에 기본 공격에 닿았을 때 HP가 감소하는 코드를 작성합니다.
+        }
+        if (other.CompareTag("Mine_Explosion"))
+        {
+            // 여기에 지뢰에 닿았을 때 HP가 감소하는 코드를 작성합니다.
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Jangpung"))
+        {
+            _isKnockback = true;
+            this.GetComponent<NavMeshAgent>().enabled = false;
+            // 여기에 몬스터가 장풍에 닿았을 때 HP가 감소하는 코드를 작성합니다.
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,6 +33,19 @@ public class PlayerController : MonoBehaviour
     public float _dashCoolDown;
     public bool _isDash = false;
 
+    public GameObject _bubblePrefab;
+    public float _bubbleCoolDown;
+
+    public GameObject _harpoonPrefab;
+    public float _harpoonCoolDown;
+
+    public GameObject _jangpungPrefab;
+    public float _jangpungCoolDown;
+    public bool _jangpungOn = false;
+
+    public GameObject _minePrefab;
+    public float _mineCoolDown;
+
     [Range(0, 100)] public int _playerHp = 100;
 
     // 플레이어의 NavMeshAgent 컴포넌트를 가져옵니다.
@@ -74,6 +87,51 @@ public class PlayerController : MonoBehaviour
             // 애니메이션이 끝나면, 플레이어의 공격 이펙트를 비활성화합니다.
             StartCoroutine(AttackEffect());
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (_harpoonCoolDown <= 0)
+            {
+                _harpoonCoolDown = 8f;
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                float angle = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
+                GameObject harpoon = Instantiate(_harpoonPrefab, transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+                Destroy(harpoon, 5f);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (_bubbleCoolDown <= 0)
+            {
+                _bubbleCoolDown = 15f;
+                GameObject bub = Instantiate(_bubblePrefab, transform.position, Quaternion.identity);
+                Destroy(bub, 2f);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_jangpungCoolDown <= 0)
+            {
+                _jangpungOn = true;
+                _jangpungCoolDown = 13f;
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                float angle = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
+                GameObject jang = Instantiate(_jangpungPrefab, transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+                Destroy(jang, 1f);
+                _jangpungOn = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (_mineCoolDown <= 0)
+            {
+                _mineCoolDown = 30f;
+                GameObject mine = Instantiate(_minePrefab, transform.position, Quaternion.identity);
+            }
+        }
         #endregion
 
         // 플레이어의 이동 속도를 설정합니다.
@@ -103,6 +161,22 @@ public class PlayerController : MonoBehaviour
         if (_dashCoolDown > 0)
         {
             _dashCoolDown -= Time.deltaTime;
+        }
+        if (_bubbleCoolDown > 0)
+        {
+            _bubbleCoolDown -= Time.deltaTime;
+        }
+        if (_harpoonCoolDown > 0)
+        {
+            _harpoonCoolDown -= Time.deltaTime;
+        }
+        if (_jangpungCoolDown > 0)
+        {
+            _jangpungCoolDown -= Time.deltaTime;
+        }
+        if (_mineCoolDown > 0)
+        {
+            _mineCoolDown -= Time.deltaTime;
         }
 
         // 플레이어가 돌진할 수 있는 상태라면, 플레이어의 투명도를 낮추고, 몬스터와의 충돌을 무시합니다.
