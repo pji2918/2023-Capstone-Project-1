f47b0fd54c39700b788515a3f16bef3f4eae5055
ask217 <qwerask2725@naver.com>
2023-05-11 18:10:17 +0900

    재료 증가 및 감소, 음식 게이지 및 제작

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public int foodCount = 0;
+
+    public Dictionary<string, int> resources = new Dictionary<string, int>()
+    {
+        {"iron", 10},
+        {"concrete", 10},
+        {"bolt", 10},
+        {"core", 10},
+        {"food", 10},
+        {"ingredient", 10}
+    };
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            GameObject go = GameObject.Find("GameManager");
+            if (go == null)
+            {
+                go = new GameObject { name = "GameManager" };
+                go.AddComponent<GameManager>();
+            }
+
+            DontDestroyOnLoad(go);
+            instance = go.GetComponent<GameManager>();
+        }
+    }
+
     void Start()
     {
         
@@ -12,5 +43,15 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void IncreseResource(string resourceName, int amount)
+    {
+        resources[resourceName] += amount;
+    }
+
+    public void ResourceReduction(string resourceName, int amount)
+    {
+        resources[resourceName] -= amount;
     }
 }
--- Assets/Scripts/Management/UIManager.cs
+++ Assets/Scripts/Management/UIManager.cs
@@ -7,111 +7,213 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-
-    [SerializeField]
+    #region 팝업창
+    [SerializeField]//업그레이드창
     private GameObject upgradeWindow;
-    [SerializeField]
+    [SerializeField]//무기 업그레이드창
     private GameObject weaponWindow;
-    [SerializeField]
-    private TextMeshProUGUI upgradeText;
-    [SerializeField]
-    private GameObject needWeaponResource;
-    [SerializeField]
+    [SerializeField]//집 업그레이드창
     private GameObject houseWindow;
-    [SerializeField]
-    private GameObject needHouseResource;
-    [SerializeField]
-    private TextMeshProUGUI buildText;
-    [SerializeField]
+    [SerializeField]//스토리 창
     private GameObject bookWindow;
-    [SerializeField]
+    [SerializeField]//설정창
     private GameObject Option;
+    #endregion
+
+    #region 텍스트
+    [SerializeField]//강화중...타이핑 텍스트
+    private TextMeshProUGUI upgradeText;
+    [SerializeField]//건축중... 타이핑 텍스트
+    private TextMeshProUGUI buildText;
+    [SerializeField]//음식 제작 상태확인 텍스트
+    private TextMeshProUGUI foodText;
+    [SerializeField]//제작 완료된 음식 개수 확인 텍스트
+    private TextMeshProUGUI foodCountText;
+    #endregion
+
+    #region 기타
+    [SerializeField]//무기 강화시 필요한 재료 확인 텍스트 묶음(빈 오브젝트)
+    private GameObject needWeaponResource;
+    [SerializeField]//집 강화시 필요한 재료 확인 텍스트 묶음(빈 오브젝트)
+    private GameObject needHouseResource;
+    [SerializeField]//재료 개수 확인 텍스트의 부모 오브잭트(빈 오브잭트)
+    private GameObject ResourceTextsParent;
+
+    //음식 제작 게이지
+    private Slider foodSlider;
+
+    //재료 개수 확인 텍스트 모음
+    private TextMeshProUGUI[] resourceTexts = new TextMeshProUGUI[6];
+
+    //음식 제작 시간
+    private float cookingTime = 30;
+    //현재 제작 시간
+    private float currentCookingTime = 0;
+    //제작 상태
+    private bool isCooking = false;
+    #endregion
+
+    private void Start()
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            resourceTexts[i] = ResourceTextsParent.transform.GetChild(i).GetComponent<TextMeshProUGUI>();
+            Debug.Log(resourceTexts[i].name);
+        }
+
+        foodSlider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        ShowResource();
+        if(isCooking)
+        {
+            currentCookingTime += Time.deltaTime;
+
+            foodSlider.value = currentCookingTime / cookingTime;
+
+            foodText.text = "음식 제작중... 남은 시간 : " + (int)(cookingTime - currentCookingTime);
+
+            if (currentCookingTime >= cookingTime)
+            {
+                StartCoroutine(TextClear(foodText, "제작이 완료되었습니다"));
+
+                GameManager.instance.foodCount++;
+                foodSlider.value = 0;
+                isCooking = false;
+                currentCookingTime = 0;
+            }
+        }
+    }
 
     #region 버튼 함수
+
+    //업그레이드 창 팝업
     public void OnClickUpgradeButton()
     {
         WindowPopUp(upgradeWindow);
     }
 
+    //무기 업그레이드 창 팝업
     public void OnClickWeaponButton()
     {
         PopUpWidowChange(weaponWindow, upgradeWindow);
     }
 
+    //무기 강화 버튼 클릭(아직 아이템 제거하는거 없음)
     public void OnClickweaponUpgradeButton()
     {
         PopUpWidowChange(upgradeText.gameObject, needWeaponResource);
         StartCoroutine(Typing(upgradeText, "강 화 중 . . .", 0.5f, upgradeText.gameObject, needWeaponResource));
     }
 
+    //집 업그레이드 창 팝업
     public void OnClickHouseButton()
     {
         PopUpWidowChange(houseWindow, upgradeWindow);
     }
 
+    //집 강화 버튼 클릭(위와 같이 아직 기능 부족)
     public void OnClickHouseUpgradeButton()
     {
         PopUpWidowChange(buildText.gameObject, needHouseResource);
         StartCoroutine(Typing(buildText, "건 설 중 . . .", 0.5f, buildText.gameObject, needHouseResource));
     }
 
+    //음식 제작(재료 아이템 제거하는거 있음)
     public void OnClickFoodButton()
     {
+        if(GameManager.instance.resources["ingredient"] >= 5 && !isCooking)
+        {
+            GameManager.instance.ResourceReduction("ingredient", 5);
+            isCooking = true;
+        }
+        else
+        {
+            StartCoroutine(TextClear(foodText, "재료가 부족합니다..."));
+        }
+        
         Debug.Log("식량제작버튼 클릭");
     }
 
+    //식량 회수
     public void OnClickFoodRecallButton()
     {
+        GameManager.instance.IncreseResource("food", GameManager.instance.foodCount);
         Debug.Log("식량회수버튼 클릭");
+        GameManager.instance.foodCount = 0;
     }
 
+    //스토리 팝업
     public void OnClickTableButton()
     {
         WindowPopUp(bookWindow);
     }
 
+    //설정 팝업
     public void OnClickOption()
     {
         WindowPopUp(Option);
     }
 
+    //업그레이드 팝업 나가기
     public void OnClickUpgradeBackground()
     {
         WindowDisappear(upgradeWindow);
     }
 
+    //스토리 팝업 나가기
     public void OnClickTableBackground()
     {
         WindowDisappear(bookWindow);
     }
 
+    //설정 팝업 나가기
     public void OnClickOptionBackground()
     {
         WindowDisappear(Option);
     }
     #endregion
 
+    //팝업 하나 키고 하나 끄기
     void PopUpWidowChange(GameObject popUp, GameObject window)
     {
         popUp.SetActive(!popUp.activeSelf);
         window.SetActive(!window.activeSelf);
     }
 
+    //팝업 끄기
     void WindowDisappear(GameObject popUp)
     {
         popUp.SetActive(false);
     }
 
+    //팝업 시키기
     void WindowPopUp(GameObject popUp)
     {
         popUp.SetActive(true);
     }
 
+    //씬 넘기기
     public void HomeToFighting()
     {
         SceneManager.LoadScene("Fighting");
     }
 
+    //식량 개수 표시
+    public void ShowResource()
+    {
+        resourceTexts[0].text = GameManager.instance.resources["iron"].ToString();
+        resourceTexts[1].text = GameManager.instance.resources["concrete"].ToString();
+        resourceTexts[2].text = GameManager.instance.resources["bolt"].ToString();
+        resourceTexts[3].text = GameManager.instance.resources["core"].ToString();
+        resourceTexts[4].text = GameManager.instance.resources["food"].ToString();
+        resourceTexts[5].text = GameManager.instance.resources["ingredient"].ToString();
+        foodCountText.text = GameManager.instance.foodCount.ToString();
+    }
+
+    //타이핑 효과 코루틴
     IEnumerator Typing(TextMeshProUGUI typingText, string message, float speed, GameObject popUp, GameObject window)
     {
         for (int i = 0; i < message.Length; i++)
@@ -122,5 +224,13 @@ public class UIManager : MonoBehaviour
         typingText.text = "완료";
         yield return new WaitForSeconds(1);
         PopUpWidowChange(popUp, window);
+    }
+
+    //텍스트 1초 띄우기
+    IEnumerator TextClear(TextMeshProUGUI text, string messege)
+    {
+        text.text = messege;
+        yield return new WaitForSeconds(1f);
+        text.text = " ";
     }
 }
